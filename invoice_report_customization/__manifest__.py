1b258d7e81f731625ffcba26b22062708c2fe3ce
Mital <mital@deardigital.be>
2022-07-18 21:20:55 +0530

    [ADD] invoice changes

---
+++ invoice_report_customization/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Invoice Report Customization',
+    'category': 'Invoice',
+    'summary': 'Invoice Report Customization',
+    'description': 'Invoice Report Customization',
+    'version': '15.0.1.0.1',
+    'author': 'dear digital',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
